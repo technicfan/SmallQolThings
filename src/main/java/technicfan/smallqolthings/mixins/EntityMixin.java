b74742a9e84b8add7097333c92be4ddeef897a68
technicfan <107669823+Technicfan@users.noreply.github.com>
2026-08-21 22:12:59 +0200

    update to 26.1.2

--- src/main/java/technicfan/smallqolthings/mixins/EntityMixin.java
+++ src/main/java/technicfan/smallqolthings/mixins/EntityMixin.java
@@ -4,26 +4,20 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.world.entity.Entity;
 
 @Mixin(Entity.class)
-public class EntityMixin {
+public abstract class EntityMixin {
     @Shadow
-    public void clearFire() {
-        throw new AssertionError();
-    }
+    private boolean wasTouchingWater;
 
-    @Inject(
-        method = "updateInWaterStateAndDoWaterCurrentPushing",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/world/entity/Entity;resetFallDistance()V",
-            shift = At.Shift.AFTER
-        )
-    )
-    private void dontBurnInWater(CallbackInfo ci) {
-        this.clearFire();
+    @Shadow
+    public abstract void clearFire();
+
+    @Inject(method = "fireImmune", at = @At("HEAD"), cancellable = true)
+    private void dontBurnInWater(CallbackInfoReturnable<Boolean> cir) {
+        if (wasTouchingWater) cir.setReturnValue(true);
     }
 }
