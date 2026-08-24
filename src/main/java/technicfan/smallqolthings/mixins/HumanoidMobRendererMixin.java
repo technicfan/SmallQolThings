ee6e9dfdaf05772e1f73f7af1d456dc4d5b60aae
technicfan <107669823+Technicfan@users.noreply.github.com>
2026-08-24 02:40:35 +0200

    hide all armor

--- src/main/java/technicfan/smallqolthings/mixins/HumanoidMobRendererMixin.java
+++ src/main/java/technicfan/smallqolthings/mixins/HumanoidMobRendererMixin.java
@@ -5,7 +5,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
@@ -14,9 +13,8 @@ import net.minecraft.world.item.ItemStack;
 @Mixin(HumanoidMobRenderer.class)
 public abstract class HumanoidMobRendererMixin {
     @Inject(method = "getEquipmentIfRenderable", at = @At("RETURN"), cancellable = true)
-    private static void hideHelmet(LivingEntity entity, EquipmentSlot slot, CallbackInfoReturnable<ItemStack> cir) {
-        // hide the helmet but keep the carved pumpkin with help of the max stack size
-        if (EquipmentSlot.HEAD.equals(slot) && entity == Minecraft.getInstance().player && cir.getReturnValue().getMaxStackSize() == 1) {
+    private static void hideArmor(LivingEntity entity, EquipmentSlot slot, CallbackInfoReturnable<ItemStack> cir) {
+        if (slot.getType() == EquipmentSlot.Type.HUMANOID_ARMOR && slot.isArmor()) {
             cir.setReturnValue(ItemStack.EMPTY);
         }
     }
