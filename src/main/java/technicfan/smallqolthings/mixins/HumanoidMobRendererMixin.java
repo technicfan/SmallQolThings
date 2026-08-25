ef52bb95a84e41ed7270987cf9727a9728975bf5
technicfan <107669823+Technicfan@users.noreply.github.com>
2026-08-25 04:33:40 +0200

    still show elytra as it looks weird otherwise

--- src/main/java/technicfan/smallqolthings/mixins/HumanoidMobRendererMixin.java
+++ src/main/java/technicfan/smallqolthings/mixins/HumanoidMobRendererMixin.java
@@ -9,12 +9,13 @@ import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 @Mixin(HumanoidMobRenderer.class)
 public abstract class HumanoidMobRendererMixin {
     @Inject(method = "getEquipmentIfRenderable", at = @At("RETURN"), cancellable = true)
     private static void hideArmor(LivingEntity entity, EquipmentSlot slot, CallbackInfoReturnable<ItemStack> cir) {
-        if (slot.getType() == EquipmentSlot.Type.HUMANOID_ARMOR && slot.isArmor()) {
+        if (slot.isArmor() && !cir.getReturnValue().is(Items.ELYTRA)) {
             cir.setReturnValue(ItemStack.EMPTY);
         }
     }
