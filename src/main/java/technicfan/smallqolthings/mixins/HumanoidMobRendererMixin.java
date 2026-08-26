39a50ac060344ded4c9d849a0a92d56ce5be0aad
technicfan <107669823+Technicfan@users.noreply.github.com>
2026-08-27 01:39:50 +0200

    only do this on the player

--- src/main/java/technicfan/smallqolthings/mixins/HumanoidMobRendererMixin.java
+++ src/main/java/technicfan/smallqolthings/mixins/HumanoidMobRendererMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
@@ -15,7 +16,7 @@ import net.minecraft.world.item.Items;
 public abstract class HumanoidMobRendererMixin {
     @Inject(method = "getEquipmentIfRenderable", at = @At("RETURN"), cancellable = true)
     private static void hideArmor(LivingEntity entity, EquipmentSlot slot, CallbackInfoReturnable<ItemStack> cir) {
-        if (slot.isArmor() && !cir.getReturnValue().is(Items.ELYTRA)) {
+        if (entity instanceof Player && slot.isArmor() && !cir.getReturnValue().is(Items.ELYTRA)) {
             cir.setReturnValue(ItemStack.EMPTY);
         }
     }
